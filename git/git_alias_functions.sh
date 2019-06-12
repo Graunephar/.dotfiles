a5ab9f4bdee6389e008d969a635aa2df60cff1ba
Daniel Graungaard <daniel@graungaard.com>
2019-06-12 15:13:07 +0200

    Added everything

---
+++ git/git_alias_functions.sh
@@ -0,0 +1,21 @@
+
+#Dangarous stuff, when you want it ask first
+function ask() {
+
+	read -p "$1" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^["$2"]$ ]]
+	then
+	    return 0
+	fi
+	return 1
+
+}
+
+
+function gitrevertdangerous {
+
+ask 'Are you sure? (y/n) ' "Yy" && ask "This will force push to the repo. Deleting the last commit entirely. Are you completely sure? (y/n) " "Yy" && ask "Every collaborator might get problems from this. If you are not collaborating with a small team you should really find another option.  Do you want to abort? (y/n) " "nN" && git reset --soft HEAD^ && git push origin +master 
+
+}
+
---
+++ git/git_custom.sh
@@ -0,0 +1,13 @@
+
+# Make sure you actually have those aliases defined, of course.
+alias g="git"
+alias gc="git checkout"
+alias gm="git merge"
+alias gp="git push"
+alias gpp="git pull"
+
+
+#Aliasing hub from github to be used with the git command
+eval "$(hub alias -s)" 
+
+
